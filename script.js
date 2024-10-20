cf7a12a63647d463500cb3585577a9ceca457fc2
sheheryar240488 <47241751+sheheryar240488@users.noreply.github.com>
2024-10-20 16:58:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            if (count > 0) {
+                count--;
+            }
+        }
+        else if (styles.contains("increase")) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        value.textContent = count;
+    });
+});
